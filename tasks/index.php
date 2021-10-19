ac1b4ac97d62c70f92a04b3209c0fca95f2739ff
Tetyana Dotsenko <atieshka@gmail.com>
2021-10-19 18:19:31 +0300

    add template content-home.php

--- tasks/index.php
+++ tasks/index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
 
-
+<?php get_template_part('templates/content-home')?>
 
 <?php get_footer(); ?>
---
+++ tasks/templates/content-home.php
@@ -0,0 +1,14 @@
+<div class="container container-home">
+	<div class="row row-home">
+		<div class="col col-home">
+			<a class="btn btn-primary btn-tasks"
+			   href="<?php echo get_site_url() . '/add'; ?>"
+			   role="button">Add New Task</a>
+		</div>
+		<div class="col col-home">
+			<a class="btn btn-primary btn-tasks"
+			   href="<?php echo get_site_url() . '/list'; ?>"
+			   role="button">View Tasks</a>
+		</div>
+	</div>
+</div>
