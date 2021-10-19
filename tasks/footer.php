a4ef0cf0183c4740f2c487d1041c5a06218153d7
Tetyana Dotsenko <atieshka@gmail.com>
2021-10-19 18:11:30 +0300

    add header.php and footer.php

---
+++ tasks/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer fixed-bottom">
+	<div class="container-xxl">
+		<div class="row justify-content-center">
+			<div class="col-sm site-footer">
+				&copy mywpsite 2021
+			</div>
+		</div>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
