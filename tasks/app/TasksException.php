05bf8778963fe2058d6c16533c77787c568755a8
Tetyana Dotsenko <atieshka@gmail.com>
2021-10-19 18:40:19 +0300

    add TasksException.php

---
+++ tasks/app/TasksException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app;
+
+class MyException extends \Exception {
+	protected array $messages = [
+		0 => 'This page isn`t exist',
+		1 => 'Site under maintenance',
+	];
+
+	public function __construct($message = "", $code = 0)
+	{
+		parent::__construct($message, $code);
+
+		// generating a message for the log
+		$error = $this->getMessage() ? $this->getMessage() : $this->messages[$this->getCode()];
+		$error .= "\r\n" . 'file ' . $this->getFile() . "\r\n" . 'in line ' . $this->getLine() . "\r\n";
+
+		// message for the user: check project message list
+		if ($this->messages[$this->getCode()]) {
+			$this->message = $this->messages[$this->getCode()];
+		}
+	}
+}
--- tasks/app/autoloader.php
+++ tasks/app/autoloader.php
@@ -3,6 +3,7 @@
 // class autoloading
 spl_autoload_register(function ($class_name) {
 	$class_name = str_replace('\\', '/', $class_name);
-	// TODO add exceptions
-	include_once __DIR__ . "/../" . $class_name . '.php';
+	if (!@include_once __DIR__ . "/../" . $class_name . '.php') {
+		throw new \app\MyException("Cannot include class " . $class_name . ". Check file name and make sure that file exists");
+	}
 });
