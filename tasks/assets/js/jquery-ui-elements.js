511849b35f35614f0adb45b0142de9f92b600524
Tetyana Dotsenko <atieshka@gmail.com>
2021-10-21 11:45:27 +0300

    edit EditPage: confirm deleting

--- tasks/assets/js/jquery-ui-elements.js
+++ tasks/assets/js/jquery-ui-elements.js
@@ -1,9 +1,21 @@
 jQuery(document).ready( function( $ ) {
     $( "input[type='radio']" ).checkboxradio({icon: false});
-});
-
-jQuery(document).ready( function( $ ) {
     $( "#datepicker" ).datepicker({
         minDate: 0
     });
+
+
+    let isConfirm = false
+    $('#upBtn').click(function() {
+        $('input[name="editBtn"]').val('update')
+        $('form#taskList').submit()
+    })
+
+    $('#delBtn').click(function() {
+        let isConfirm = confirm("Are you sure?")
+        if (isConfirm) {
+            $('input[name="editBtn"]').val('delete')
+            $('form#taskList').submit()
+        }
+    })
 });
